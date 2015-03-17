8fac3308217fa4964f8034b1ed9c0e939ec768dc
Brandon Xu <findthegateopen@gmail.com>
2015-03-17 02:47:14 -0700

    starting chapter 3

--- Kaleidoscope/toy.cpp
+++ Kaleidoscope/toy.cpp
@@ -4,6 +4,13 @@
 #include <map>
 #include <string>
 #include <vector>
+#include "llvm/IR/Verifier.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+
+using namespace llvm;
 
 enum Token {
   tok_eof = -1,
